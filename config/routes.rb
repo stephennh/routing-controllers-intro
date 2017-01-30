bc81138a67808bebb4da5571e1e477fe72cc0729
Stephen H <stephen210huang@hotmail.com>
2017-01-30 13:35:51 -0500

    /contest page working

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  get "/" => "pages#welcome"
+  get "/"           => "pages#welcome"
 
-  get "/welcome" => "pages#welcome"
+  get "/welcome"    => "pages#welcome"
 
-  get "/about" => "pages#about"
+  get "/about"      => "pages#about"
 
+  get "/contest"    => "pages#contest"
 end
