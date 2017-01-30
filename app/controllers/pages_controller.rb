8115da40c77320ed689b51fffc6bbbb4e0063811
Stephen H <stephen210huang@hotmail.com>
2017-01-30 13:38:03 -0500

    instance variable @header completed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,15 @@
 class PagesController < ApplicationController
 
   def welcome
-
+    @header = "This is the welcome page"
   end
 
   def about
-
+    @header = "This is the about us page"
   end
 
   def contest
-
+    @header = "This is the contest page"
   end
 
 end
